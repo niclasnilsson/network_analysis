826fc0a00a8758888298aeb864a60d8bf5d1369d
Niclas Nilsson <niclas@niclasnilsson.se>
2014-02-16 14:36:20 +0100

    Initial commit

---
+++ ping.rb
@@ -0,0 +1,19 @@
+require 'net/ping'
+ 
+def ping(ip)
+  icmp = Net::Ping::ICMP.new(ip)
+  loop do
+    start = Time.now
+    duration_format, duration = icmp.ping ? ['%0.3f', icmp.duration * 1000] : ['%s', 'timeout']
+    puts sprintf "%s, #{duration_format}, %s", icmp.host, duration, Time.now
+
+    took = Time.now - start
+    left = 1 - took
+
+    if left > 0
+      sleep left
+    end
+  end
+end
+
+ping ARGV[0]
